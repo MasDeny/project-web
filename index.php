079b611f827d9ea2d2a1c636afe0cfdd9fb701a1
MasDeny <kickdeny@gmail.com>
2016-12-30 11:04:04 +0700

    Update index.php
    solved merge confict

--- index.php
+++ index.php
@@ -1,68 +1,66 @@
 
 <?php
-	include ("cek.php");
+  include ("cek.php");
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	
+  
 
-	<title>POLIJE - Homepage</title>
-	
-	<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
-	<script src="jquery/jquery-1.11.2.min.js"></script>
-	<script src="bootstrap/js/bootstrap.js"></script>
+  <title>PERPOL - Homepage</title>
+  
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  
+  <style type="text/css">
 
-	<style type="text/css">
-
-		/*to make the background image responsive and cover the page*/
-		body{
-			background-size: cover;
-		}
-	</style>
+    /*to make the background image responsive and cover the page*/
+    body{
+      background-size: cover;
+    }
+  </style>
 
 </head>
 <body background="image/bg_lary.png">
 
-	<!-- navigation bar -->
-	<nav class="navbar navbar-inverse">
-	  <div class="container-fluid">
-	    <div class="navbar-header">
-	      <a class="navbar-brand" href="index.php">POLIJE Library Automation</a>
-	    </div>
-	    <div>
-	      <ul class="nav navbar-nav">
-	        <li class="active"><a href="">HOME</a></li>
-	        <li><a href="search.php">DAFTAR BUKU</a></li>
-	        <li><a href="about.php">ABOUT</a></li>
+  <!-- navigation bar -->
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="index.php">Perpustakaan POLIJE</a>
+      </div>
+      <div>
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="">Home</a></li>
+          <li><a href="search.php">Daftar Buku</a></li>
+          <li><a href="about.php">About</a></li>
 
-	      </ul>
-		  <ul class="nav navbar-nav navbar-right">
+        </ul>
+      <ul class="nav navbar-nav navbar-right">
 
-			//<a href="login-page.php">
-				<button type="button" class="btn btn-primary btn-md">
-				<span class="glyphicon glyphicon-log-in"></span> Login Admin
-				</button>
-			</a>
-		  
-	     </ul>
-	    </div>
-	  </div>
-	</nav>
-
-
+      <a href="login-page.php">
+        <button type="button" class="btn btn-primary btn-md">
+        <span class="glyphicon glyphicon-log-in"></span> Login Admin
+        </button>
+      </a>
+      
+       </ul>
+      </div>
+    </div>
+  </nav>
 	
 	<div class="container">
 		<!-- body text -->
 		<div class="jumbotron">
 			<img src="image/logo-box.png" width="550"height="140">
-<h1 class="page-header">WELCOME to POLIJE library </h1>
-			<p>POLIJE LA atau <strong>POLIJE Library Automation</strong> adalah layanan perpustakaan pusat Politeknik Negeri Jember
-			yang ditujukan untuk melayani civitas akademika POLIJE.</p>
-			<p>Untuk melakukan registrasi keanggotaan dan peminjaman serta pengembalian buku, kunjungi perpustakaan kami pada 
-			hari kerja. Pencarian buku dapat dilakukan di bawah ini.</p>
-		</div>
+<h1 class="page-header">Selamat Datang di Perpustakaan POLIJE</h1>
+      <p>PERPOL atau <strong>Perpustakaan POLIJE</strong> adalah layanan perpustakaan pusat Politeknik Negeri Jember yang ditujukan untuk mempermudah admin perpustakaan POLIJE.</p>
+      <p>Untuk melakukan registrasi keanggotaan dan peminjaman serta pengembalian buku, kunjungi perpustakaan kami pada 
+      hari kerja. Pencarian buku dapat dilakukan di bawah ini.</p>
+    </div>
 
 		<!-- form pencarian buku -->
 		<div class="well well-lg text-center">
@@ -80,7 +78,7 @@
 				</select>
 
 				<input type="text" placeholder="Masukkan kata kunci" name="keyword" required>
-				<input type="submit" name="search" value="cari" class="btn btn-info">
+				<input type="submit" name="search" value="cari" class="btn btn-info btn-xs">
 					
 			</form>
 			
