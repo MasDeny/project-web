24cbf79792fca46f90849bb0bc124920b4f5f732
Gita-PC\Gita <gitariani1904@gmail.com>
2017-01-04 22:32:14 +0700

    hihi

---
+++ config/connection.php
@@ -0,0 +1,14 @@
+
+<?php
+	$server = "localhost";
+	$username = "root";
+	$password = "";
+	$database = "perpustakaan";
+
+	$connection = mysqli_connect($server, $username, $password, $database);
+
+	if(!$connection){
+		die("Maaf, gagal tersambung dengan database!");
+	}
+	
+?>
